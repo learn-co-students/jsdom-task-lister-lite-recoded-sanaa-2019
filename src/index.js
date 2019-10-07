0091222ce0fa6a852af5df0b65fb7f0e9a0bfea8
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-07 08:03:19 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const input = document.getElementById('new-task-description');
+  const form = document.getElementById('create-task-form');
+  const tasks = document.getElementById("tasks");
+  const priority = document.getElementById('priority');
+
+  form.addEventListener('submit',handleSubmition);
+
+  function deleteTask(e){
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+  }
+
+
+  // function sortList(list){
+  //       let coll = [...list]
+  //       if(coll.length > 2){
+  //         coll.sort(function(a, b) {
+  //         let textA = a.getAttribute('data-sort').toLowerCase()
+  //         let textB = b.getAttribute('data-sort').toLowerCase()
+  //         return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+  //       });
+  //       }
+  //   return coll;
+  // }
+
+  function handleSubmition(e){
+    e.preventDefault();
+    if(input.value != ""){
+      let option = priority.options[priority.selectedIndex];
+      let sortChar = option.getAttribute('data-sort');
+      let li = document.createElement('li');
+      let btn = document.createElement('button');
+      li.textContent = input.value + ' ';
+      li.setAttribute('data-sort',sortChar);
+      li.style.color = priority.value;
+      btn.innerHTML = 'X';
+      btn.addEventListener('click',deleteTask);
+      li.appendChild(btn);
+      tasks.appendChild(li);
+      // let liList = tasks.getElementsByTagName('li');
+      // let sortedList = sortList(liList);
+      // console.log(sortedList);
+      input.value = "";
+    }
+  }
 });
