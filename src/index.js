3be6b0815831da261bed13119c41493e146a0ef9
Firas Al-Azizy <firasazizy614@gmail.com>
2019-10-25 22:28:32 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.body.addEventListener("DOMContentLoaded", () => {
+  function addLi(input) {
+    let uList = document.getElementById('tasks');
+    let element = document.createElement('li');
+    element.innerHTML = input;
+    uList.appendChild(element);
+  }
+  let a=document.body.querySelector("#create-task-form input[type='submit']")
+  a.addEventListener("click", function (e) {
+    e.preventDefault();
+    let input = document.getElementById('new-task-description').value;
+    addLi(input);
+    input.value = "";
+
+  });
 });
