fa1bb0f9d6b250b2bae3206124bc2f05ae3f8ce8
AimanAlmureish <aiman.almureish@gmail.com>
2019-10-31 13:35:49 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let input = document.getElementById("new-task-description");
+  let submit = document.getElementById("submit");
+  submit.onclick = function (e) {
+    let tasks = document.getElementById("tasks");
+    let li = document.createElement("li")
+    li.innerHTML = input.value;
+    tasks.appendChild(li);
+    e.preventDefault();
+  }
 });
+
+
