df32123a41d9660ad18455a26855bd4c3f50057e
TaghreedNageeb <taghreed.n.d2018@gmail.com>
2019-11-03 03:24:50 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+let btn= document.querySelector('submit');
+
+btn.addEventListener('click',function(){
+    console.log("taghreed")
+})
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let btn = document.getElementById("btn");
+  btn.onclick = function (e) {
+    let task = document.getElementById("new-task-description").value;
+    let taskList = document.getElementById("tasks");
+    let newLi = document.createElement("li")
+    let content= document.createTextNode(task);
+    newLi.appendChild(content);
+    taskList.appendChild(newLi);
+    e.preventDefault();
+  }
 });
+
