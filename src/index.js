7e35b2f3b6435e1946c2f06fffdaa93596a0e4b0
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-01 12:46:48 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const input = document.getElementById('new-task-description');
+  const form = document.getElementById('create-task-form');
+  const tasks = document.getElementById("tasks");
+  const priority = document.getElementById('priority');
+  
+  
+  function list(input) {
+    
+    let li = document.createElement('li');
+    li.innerHTML = input;
+    tasks.appendChild(li);
+  }
+  
+
+form.addEventListener("onClick", function (e) {
+e.preventDefault();
+
+list(input);
+input.value = " ";
+
+  });
+
 });
